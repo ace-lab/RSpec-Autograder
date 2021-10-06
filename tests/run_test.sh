047ac1d2d2f3ee9fdc345c7de63f6b71dc781d0c
nelson lojo <nalsonklojo@gmail.com>
2021-10-05 22:56:34 -0700

    PLEASE LET RSPEC FIND MY CASES

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -2,15 +2,15 @@
 
 # $1 is suites_dir
 
-sudo apt update
-sudo apt install -y rbenv
-eval "$(rbenv init -)"
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
-echo The following install may take up to 30 mins, feel free to grab a coffe
-rbenv install 2.7.1
-rbenv rehash
-gem install bundle
+# sudo apt update
+# sudo apt install -y rbenv
+# eval "$(rbenv init -)"
+# mkdir -p "$(rbenv root)"/plugins
+# git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+# echo The following install may take up to 30 mins, feel free to grab a coffe
+# rbenv install 2.7.1
+# rbenv rehash
+# gem install bundle
 
 # make all the necessary dirs
 mkdir .testing
@@ -44,8 +44,8 @@ chmod +x .testing/grader/run.py
 # and run it
 echo ==================== RUNNING THE GRADER ====================
 echo
-.testing/grader/run.py `pwd`/.testing
-# sudo docker build .
+# .testing/grader/run.py `pwd`/.testing
+sudo docker build .
 echo 
 echo ========================= FINISHED =========================
 
